b5089e957807d2f239fd5ce28b70b304193a3e10
drenbytyqi <db47104@ubt-uni.net>
2022-06-03 13:41:51 +0200

    commit 2

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ module.exports ={
     domains: ["links.papareact.com", "image.tmdb.org"],
   },
   env:{
-    MONGO_URI: "mongodb+srv://DrenBytyqi:dbuser@cluster0.zh9ev.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URI: "mongodb+srv://DrenBytyqi:dbuser@cluster0.zh9ev.mongodb.net/?retryWrites=true&w=majority",
+    API_KEY: "4395adddaa8e2cdc8de102be2589d4b2",
+
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyBO_HsoH-cAoZ9CLiTQKP9a16hua6sBmZo",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "hulu-project-545dc.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "hulu-project-545dc",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCCKET: "hulu-project-545dc.appspot.com",
+    NEXT_PUBLIC_FIREBASE_MESSAFINF_SENDER_ID: "1028619835800",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:1028619835800:web:8111e22f935dff877f33ed",
   }
 };
 
